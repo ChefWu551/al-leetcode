232e59ac414849ad795f1d281e0cf420e0fbdcff
wuyuefeng <565948592@qq.com>
2021-09-08 10:00:17 +0800

    添加链表翻转

---
+++ src/main/java/com/yuefeng/ArrayAndLink.java
@@ -0,0 +1,13 @@
+package main.java.com.yuefeng;
+
+import java.util.LinkedList;
+
+// 数组和链表基础
+public class ArrayAndLink {
+
+    public static void main(String[] args) {
+        int[] values = new int[10];
+
+        LinkedList list = new LinkedList();
+    }
+}
