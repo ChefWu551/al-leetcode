09da37326e16ef75f385550c7f464e76a4c7d23a
wuyuefeng <565948592@qq.com>
2021-10-06 09:41:37 +0800

    添加二叉树有序打印数组

--- src/main/java/com/yuefeng/bTreePractice/DeleteNode.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.yuefeng.bTreePractice;
-
-// https://leetcode-cn.com/problems/delete-node-in-a-bst/
-public class DeleteNode {
-}
--- src/main/java/com/yuefeng/bTreePractice/LevelOrder.java
+++ src/main/java/com/yuefeng/bTreePractice/LevelOrder.java
@@ -1,5 +1,39 @@
 package main.java.com.yuefeng.bTreePractice;
 
+import main.java.com.yuefeng.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
 public class LevelOrder {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(50, new TreeNode(30, null, new TreeNode(40)), new TreeNode(70, new TreeNode(60), new TreeNode(80)));
+        List<Integer> values = new ArrayList<>();
+        LevelOrder order = new LevelOrder();
+        order.levelOrder(root);
+        System.out.println(values);
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        int level = 0;
+        preOrder(result, root, level);
+        return result;
+    }
+
+   public List<List<Integer>> preOrder(List<List<Integer>> values, TreeNode node, int level) {
+
+        if (node == null) {
+            return values;
+        }
+
+        if (values.size() == level) values.add(level, new ArrayList<>());
+        values.get(level).add(node.val);
+        level++;
+        preOrder(values, node.left, level);
+        preOrder(values, node.right, level);
+        return values;
+   }
 }
