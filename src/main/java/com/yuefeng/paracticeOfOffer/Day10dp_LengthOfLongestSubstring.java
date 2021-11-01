4940977fbffc544cd81ee7f8e4c9781102f64058
wuyuefeng <565948592@qq.com>
2021-11-01 22:46:50 +0800

    增加学习笔记

--- src/main/java/com/yuefeng/paracticeOfOffer/Day10dp_LengthOfLongestSubstring.java
+++ src/main/java/com/yuefeng/paracticeOfOffer/Day10dp_LengthOfLongestSubstring.java
@@ -1,6 +1,8 @@
 package main.java.com.yuefeng.paracticeOfOffer;
 
 import javax.print.DocFlavor;
+import java.util.HashMap;
+import java.util.Map;
 
 // https://leetcode-cn.com/problems/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof/
 public class Day10dp_LengthOfLongestSubstring {
@@ -9,22 +11,20 @@ public class Day10dp_LengthOfLongestSubstring {
         Day10dp_LengthOfLongestSubstring d = new Day10dp_LengthOfLongestSubstring();
         d.lengthOfLongestSubstring("abcabcbb");
     }
-    public int lengthOfLongestSubstring(String s) {
-        if (s.length() <= 1) return s.length();
-        int max = 0;
-        for (int i=0; i<s.length(); i++) {
-            int tmp = 1;
-            for (int j = i+1; j<s.length(); j++) {
-                if (s.charAt(i) != s.charAt(j)) {
-                    tmp++;
-                } else {
-                    break;
-                }
-            }
 
-            max = Math.max(max, tmp);
+    public int lengthOfLongestSubstring(String s) {
+
+        Map<Character, Integer> maps = new HashMap<>();
+        int maxLen = 0;
+        int tmpIndex = -1;
+        for (int i=0; i<s.length(); i++) {
+            char sc = s.charAt(i);
+            if (maps.containsKey(sc)) tmpIndex = Math.max(maps.get(sc), tmpIndex)  ;
+            maxLen = Math.max(maxLen, i - tmpIndex );
+            maps.put(sc, i);
+
         }
 
-        return max;
+        return maxLen;
     }
 }
--- src/main/java/com/yuefeng/paracticeOfOffer/Day20FZ_Btree_LowestCommonAncestor.java
+++ src/main/java/com/yuefeng/paracticeOfOffer/Day20FZ_Btree_LowestCommonAncestor.java
@@ -6,28 +6,25 @@ import main.java.com.yuefeng.model.TreeNode;
 public class Day20FZ_Btree_LowestCommonAncestor {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(6,
-                new TreeNode(2,
-                        new TreeNode(0),
-                        new TreeNode(4, new TreeNode(3), new TreeNode(5))),
-                new TreeNode(8,
-                        new TreeNode(3, new TreeNode(4), new TreeNode(4)), new TreeNode(3)));
+        TreeNode root = new TreeNode(3,
+                new TreeNode(5,
+                        new TreeNode(6),
+                        new TreeNode(2, new TreeNode(7), new TreeNode(4))),
+                new TreeNode(1,
+                        new TreeNode(0), new TreeNode(8)));
 
         Day20FZ_Btree_LowestCommonAncestor d = new Day20FZ_Btree_LowestCommonAncestor();
-        d.lowestCommonAncestor(root, new TreeNode(4), new TreeNode(5));
+        d.lowestCommonAncestor(root, new TreeNode(4), new TreeNode(8));
     }
 
-    TreeNode tmp;
-    int b = 0;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return root;
-
-        if (p.val > root.val && q.val > root.val)
-           return lowestCommonAncestor(root.right, p, q);
-        if (p.val < root.val && q.val < root.val)
-            return lowestCommonAncestor(root.left, p, q);
-        b++;
-        return root;
-
+        if(root == null || root.val == p.val || root.val == q.val) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) return null; // 1.
+        if(left == null) return right; // 3.
+        if(right == null) return left; // 4.
+        return root; // 2. if(left != null and right != null)
     }
+
 }
